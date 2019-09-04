c75eaa80a1b9ffcb40d1de72a71539e4307933ab
ilay00 <agidelio@mail.ru>
2019-09-04 23:02:57 +0500

    40919

---
+++ index d3.php
@@ -0,0 +1,36 @@
+<?php
+function slog($oneSlog, $twoSlog)
+{
+    $sum=$oneSlog+$twoSlog;
+    return $sum;
+}
+
+echo slog(10,10);
+
+echo "<br>";
+function vich($min,$vich)
+{
+    $raz=$min-$vich;
+    return $raz;
+}
+   echo vich(12,8);
+   echo "<br>";
+   function umnaj($oneMnoj,$twoMnoj)
+{
+    $proz=$oneMnoj*$twoMnoj;
+    return $proz;
+}
+
+ echo umnaj(2,5);
+ echo "<br>";
+
+ function deleniy($delet,$delem)
+{
+    $chast=$delet/$delem;
+    return $chast;
+}
+   echo deleniy(20,10);
+
+
+   
+?>
