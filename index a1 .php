3abff6e7d12261ae19f518df33ad88af7ac547a7
ilay00 <agidelio@mail.ru>
2019-09-07 18:32:52 +0500

    70919

---
+++ index a1 .php
@@ -0,0 +1,13 @@
+<?
+$a=0;
+while($a++ < 100) if($a%3==0)
+  {  
+    
+    print_r('<br>'); 
+    print_r($a);
+    print_r('<br>'); 
+}
+
+
+
+?>
---
+++ index a5.php
@@ -0,0 +1,11 @@
+<?php
+function zam($string)
+{
+
+  echo $zam=str_replace( ' ','_',$string );
+ return $zam;
+}
+$str="dj Hello";
+ zam($str);
+
+?>
